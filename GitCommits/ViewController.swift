e0c87ff816fb529550ded8b2ae792bc848b67ae9
Nicolaudie <adam.smith@nicolaudiegroup.com>
2018-11-24 15:35:45 +0000

    Initial Commit

---
+++ GitCommits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitCommits
+//
+//  Created by Adam Rikardsen-Smith on 24/11/2018.
+//  Copyright © 2018 Adam Rikardsen-Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
